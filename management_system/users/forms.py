f8e0c58d1c3892126ced992a028738c7a077b2f2
dessmound <55000276+dessmound@users.noreply.github.com>
2022-06-26 19:45:48 +0300

    Routs

---
+++ management_system/users/forms.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.models import User 
+from django import forms
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField()
